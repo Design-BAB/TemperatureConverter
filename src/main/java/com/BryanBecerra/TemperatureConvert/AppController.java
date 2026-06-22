e22598e6c24fa80fb6e97d99141c927278eb2a25
Bryan Becerra <87731894+Design-BAB@users.noreply.github.com>
2026-06-22 17:15:40 -0400

    Finishing rewrite
    I finished re-writing all the aspects of the original Golang application into Java

--- src/main/java/com/BryanBecerra/TemperatureConvert/AppController.java
+++ src/main/java/com/BryanBecerra/TemperatureConvert/AppController.java
@@ -4,6 +4,7 @@
  */
 package com.BryanBecerra.TemperatureConvert;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -23,13 +24,47 @@ public class AppController {
         return "<h1>Hello moon</h1>";
     }
     
-        
+    //here the return type is ResponseEntity<String> so I may actually control the HTTP status code
+    // this allows me to put some safty around the converting type
     @GetMapping("/convert")
     @ResponseBody
-    public String convert(@RequestParam String number1, @RequestParam String tempType) {
-        var newAmount = Float.parseFloat(number1);
-        Temp theTemperature = new Temp(tempType.charAt(0), newAmount);
-        return theTemperature.toString();
+    public ResponseEntity<String> convert(@RequestParam String number1, @RequestParam String tempType) {
+        double newAmount;
+        try {
+            newAmount = Double.parseDouble(number1);
+        } catch (NumberFormatException e) {
+           return ResponseEntity.badRequest().body("Invalid input. Please enter valid numbers.");
+        }
+        Temp theTemperature = new Temp(tempType, newAmount);
+        if (theTemperature.getType() == 'F') {
+            return ResponseEntity.ok(theTemperature.convert('C'));
+        } else if (theTemperature.getType() == 'C') {
+            return ResponseEntity.ok(theTemperature.convert('F'));
+        }
+        return ResponseEntity.badRequest().body("Please select a conversion type");
+}
+    
+    @GetMapping("/formLabel")
+    @ResponseBody
+    public String formLabel(@RequestParam String tempType) {
+        char type = tempType.charAt(0);
+        type = Character.toUpperCase(type);
+        var formUpdate = new StringBuilder();
+        switch(type) {
+            case 'F':
+                //using a string builder will help make the html readable having seperate lines to see
+                formUpdate.append("<label for=\"number1\">Fahrenheit: </label>");
+                formUpdate.append("<input type=\"number\" id=\"number1\" name=\"number1\" min=\"-100\"><br>");
+                break;
+            case 'C':
+                formUpdate.append("<label for=\"number1\">Celsius: </label>");
+                formUpdate.append("<input type=\"number\" id=\"number1\" name=\"number1\" min=\"-100\"><br>");
+                break;
+            default:
+                formUpdate.append("<p>Unknown temperature type</p>");
+                break;
+        }
+        return formUpdate.toString();
     }
     
 }
--- src/main/java/com/BryanBecerra/TemperatureConvert/Temp.java
+++ src/main/java/com/BryanBecerra/TemperatureConvert/Temp.java
@@ -1,58 +1,109 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+//Author: BAB-Design
+//Date 6/22/2026
+
 package com.BryanBecerra.TemperatureConvert;
 
-/**
- *
- * @author bbece
- */
+
 public class Temp {
-    private int type;
-    private float amount;
+    private char type;
+    private double amount;
     
-    public Temp(char type, float amount) {
-        this.type = type;
+    
+    public Temp(String type, double amount) {
+        var charType = type.charAt(0);
+        this.type = Character.toUpperCase(charType);
         this.amount = amount;
     }
-    public float getAmount() {
+    
+    
+    public double getAmount() {
         return amount;
     }
-    public int getType() {
+    
+    
+    public String getAmountString() {
+        var theAmount = String.format("%.0f", amount);
+        return theAmount;
+    }
+    
+    
+    public char getType() {
         return type;
     }
     
-    public void setType(int type) {
+    
+    public void setType(char type) {
+        type = Character.toUpperCase(type);
         this.type = type;
     }
+    
+    
     public void setAmount(int amount) {
         this.amount = amount;
     }
 
+    
     @Override
     public String toString() {
         return "Temp{" + "type=" + type + ", amount=" + amount + '}';
     }
      
     
-    
-    public float convert(char intoNewType){
-        //So here, we bring in what type and output the results
-        //note to Bryan, you gotta make this a switch statement
-        if (intoNewType == 'c' && type == 'f') {
-            float newF;
-            float factor = 9 / 5;
-            newF = amount * factor;
-            newF = newF + 32;
-            return newF;
-        } else if (intoNewType == 'c' && type == 'f') {
-            float newC = amount - 32;
-            float factor = 5 / 9;
-            newC = newC * factor;
-            return newC;
-        }
-        return 0;
+    private String makeItStrongHTML(String input) {
+        var output = new StringBuilder();
+        output.append("<strong>");
+        output.append(input);
+        output.append("</strong>");
+        return output.toString();
     }
     
+    
+    public String convert(char intoNewType){
+        //So here, we bring in what type and output the results
+        //Did a switch statement so we don't have to deal with all possiblities with if else
+        switch (type) {
+            //here we are converting from F
+            case 'F':
+                switch(intoNewType){
+                    case 'C':
+                        double newC = amount - 32;
+                        var factor = 5.0 / 9;
+                        newC = newC * factor;
+                        String results = "";
+                        // the purpose of this if statement is so that it looks nicer if there are no fraction of a temp
+                        if (newC % 1 == 0) {
+                            results = String.format("%.0f", newC);
+                        } else {
+                            //otherwise the hundredth's place is fine
+                            results = String.format("%.2f", newC);
+                        }
+                        results = "The temperature " + getAmountString() + " " + type + " is " + results + " C";
+                        return makeItStrongHTML(results);
+                        
+                    default:
+                        break;
+                }
+            //Here we are converting from C
+            case 'C':
+                switch(intoNewType){
+                    case 'F':
+                        double newF;
+                        var factor = 9 / 5.0;
+                        newF = amount * factor;
+                        newF = newF + 32;
+                        String results = "";
+                        //If no fraction of a temp as well
+                        if (newF % 1 == 0) {
+                            results = String.format("%.0f", newF);
+                        } else {
+                            results = String.format("%.2f", newF);
+                        }
+                        results = "The temperature " + getAmountString() + " " + type + " is " + results + "F";
+                        return makeItStrongHTML(results);
+                    default:
+                        break;
+                }
+        }
+        return "Error";
+    }
 }
